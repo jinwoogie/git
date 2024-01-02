1d6c2e0f570bf4b9971a61344d32ae511cb679cb
jinwook <woogie@koast.net>
2024-01-02 18:08:04 +0900

    cherrypick candidate2

---
+++ src/main/java/org/example/cherr.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class cherr {
+    public static void main(String[] args) {
+        System.out.println("true = " + true);
+        System.out.println("true = " + true);
+        System.out.println("true = " + true);
+        System.out.println("true = " + true);
+        System.out.println("true = " + true);System.out.println("true = " + true);
+
+    }
+}
