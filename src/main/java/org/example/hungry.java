53d540a385fe3351bf3525e025be39a9242cd2f5
jinwook <woogie@koast.net>
2024-01-02 18:07:07 +0900

    cherrypick candidate1

--- src/main/java/org/example/hungry.java
+++ src/main/java/org/example/hungry.java
@@ -1,10 +1,15 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class hungry {
     public static void main(String[] args) {
         System.out.println("배고파");
-        System.out.println("배고파");
-        System.out.println("배고파");
-        System.out.println("배고파");
+        System.out.println("args = " + Arrays.toString(args));
+        System.out.println("args = " + Arrays.toString(args));
+        System.out.println("args = " + Arrays.toString(args));
+        System.out.println("args = " + Arrays.toString(args));
+        System.out.println("args = " + Arrays.toString(args));
+        System.out.println("args = " + Arrays.toString(args));
     }
 }
