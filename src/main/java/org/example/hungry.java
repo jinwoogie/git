00846384a67a37def52a83d2a7826b8c3aa77b8b
jinwook <woogie@koast.net>
2024-04-23 16:10:59 +0900

    asdfdd

--- src/main/java/org/example/hungry.java
+++ src/main/java/org/example/hungry.java
@@ -11,5 +11,6 @@ public class hungry {
         System.out.println("args = " + Arrays.toString(args));
         System.out.println("args = " + Arrays.toString(args));
         System.out.println("args = " + Arrays.toString(args));
+        System.out.println("args = " + Arrays.toString(args));
     }
 }
